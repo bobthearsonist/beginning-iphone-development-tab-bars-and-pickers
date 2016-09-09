3dbdae91c4abe02a4ae6b960b16d1350065c1326
Martin Perry <martinperry@gatech.edu>
2016-09-09 10:55:39 -0400

    Initial date in picker

--- Pickers/Pickers/DatePickerViewController.m
+++ Pickers/Pickers/DatePickerViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    NSDate* currentDate = [NSDate date];
+    [self.datePicker setDate:currentDate];
 }
 
 - (void)didReceiveMemoryWarning {
