db5cc1ada82a21933497dbd88af611d5ab4367a2
Martin Perry <martinperry@gatech.edu>
2016-09-10 12:48:10 -0400

    Two Pickers
    Used two pickers to implement the double picker. Doesn’t Work.

--- Pickers/Pickers/DoubleComponentPickerViewController.m
+++ Pickers/Pickers/DoubleComponentPickerViewController.m
@@ -8,20 +8,92 @@
 
 #import "DoubleComponentPickerViewController.h"
 
-@interface DoubleComponentPickerViewController ()
+enum Pickers {Sandwich=0,Bread=1};
 
+@interface DoubleComponentPickerViewController ()
+@property (weak, nonatomic) IBOutlet UIPickerView *sandwichPicker;
+@property (strong, nonatomic) NSArray *sandwiches;
+
+@property (weak, nonatomic) IBOutlet UIPickerView *breadPicker;
+@property (strong, nonatomic) NSArray* breads;
 @end
 
 @implementation DoubleComponentPickerViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.sandwiches = @[@"Ham",@"Turkey"];
+    self.breads = @[@"Rye",@"Wheat"];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonPressed:(UIButton *)sender {
+    NSString* selectedSandwich = self.sandwiches[[self.sandwichPicker selectedRowInComponent:0]];
+    NSString* selectedBread = self.breads[[self.breadPicker selectedRowInComponent:0]];
+    
+    UIAlertController* halAlert = [UIAlertController alertControllerWithTitle:@"Sorry." message:@"I am afraid I cant do that." preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction* okay = [UIAlertAction actionWithTitle:@"Okay" style:UIAlertActionStyleDefault handler:nil];
+    [halAlert addAction:okay];
+    
+    //TODO find out how to define the handler seperately instead of inline
+    UIAlertAction* sudo =[UIAlertAction actionWithTitle:@"sudo sandwich" style:UIAlertActionStyleCancel handler:    ^(UIAlertAction * sudoSandwich){
+        UIAlertController* sudoController = [UIAlertController alertControllerWithTitle:@"Here's your sandwich jerk" message:[NSString stringWithFormat:@"A %@ on %@ sandiwch.",selectedSandwich,selectedBread] preferredStyle:UIAlertControllerStyleAlert];
+        [sudoController addAction:[UIAlertAction actionWithTitle:@"Thanks" style:UIAlertActionStyleCancel handler:nil]];
+        
+        [self presentViewController:sudoController animated:NO completion:nil];
+    }];
+    [halAlert addAction:sudo];
+    
+    [self presentViewController:halAlert animated:YES completion:nil];
+}
+
+#pragma mark -
+#pragma mark Picker Data Source Methods
+- (NSInteger) numberOfComponentsInPickerView:( UIPickerView *) pickerView
+{
+    return 1;
+}
+- (NSInteger) pickerView:( UIPickerView *) pickerView numberOfRowsInComponent:( NSInteger) component
+{
+    switch (component) {
+        case Sandwich:
+        {
+            return [self.sandwiches count];
+            break;
+        }
+        case Bread:
+        {
+            return [self.breads count];
+            break;
+        }
+        default:
+            return 0;
+    }
+}
+
+#pragma mark Picker Delegate Methods
+- (NSString *) pickerView:( UIPickerView *) pickerView titleForRow:( NSInteger) row forComponent:( NSInteger) component
+{
+    switch (component) {
+        case Sandwich:
+        {
+            return self.sandwiches[row];
+            break;
+        }
+        case Bread:
+        {
+            return self.breads[row];
+            break;
+        }
+        default:
+            return 0;
+    }
 }
 
 /*
