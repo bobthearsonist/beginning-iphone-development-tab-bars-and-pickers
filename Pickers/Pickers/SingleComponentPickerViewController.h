c5689a4d9c6573c15ea3f03a0dd45dbe7216cb85
Martin Perry <martinperry@gatech.edu>
2016-09-10 10:16:00 -0400

    Single Picker
    Adds a single picker with an alert for the selection.

--- Pickers/Pickers/SingleComponentPickerViewController.h
+++ Pickers/Pickers/SingleComponentPickerViewController.h
@@ -9,5 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface SingleComponentPickerViewController : UIViewController
-
+<UIPickerViewDelegate,UIPickerViewDataSource>
 @end
--- Pickers/Pickers/SingleComponentPickerViewController.m
+++ Pickers/Pickers/SingleComponentPickerViewController.m
@@ -9,19 +9,50 @@
 #import "SingleComponentPickerViewController.h"
 
 @interface SingleComponentPickerViewController ()
-
+@property (weak, nonatomic) IBOutlet UIPickerView *singlePicker;
+@property (strong,nonatomic) NSArray *characterNames;
 @end
 
 @implementation SingleComponentPickerViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.characterNames = @[@"Luke",@"Leia",@"Han",@"Chewbacca",@"Artoo",@"Threepio",@"Lando"];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonPressed:(id)sender {
+    NSInteger row = [self.singlePicker selectedRowInComponent:0];
+    NSString* selected = self.characterNames[row];
+    NSString* title = [NSString stringWithFormat:@"You selected %@",selected];
+    
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:title message:@"Thank you for choosing." preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction* action = [UIAlertAction actionWithTitle:@"youre welcome" style:UIAlertActionStyleDefault handler:nil];
+    
+    [alert addAction:action];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+
+#pragma mark - 
+#pragma mark Picker Data Source Methods 
+- (NSInteger) numberOfComponentsInPickerView:( UIPickerView *) pickerView
+{
+    return 1;
+}
+- (NSInteger) pickerView:( UIPickerView *) pickerView numberOfRowsInComponent:( NSInteger) component
+{
+    return [self.characterNames count];
+}
+#pragma mark Picker Delegate Methods 
+- (NSString *) pickerView:( UIPickerView *) pickerView titleForRow:( NSInteger) row forComponent:( NSInteger) component
+{
+    return self.characterNames[row];
 }
 
 /*
